eae7ecf91d40cddb74fc729ce1501a199c59d657
daichi1412 <149953061+daichi1412@users.noreply.github.com>
2023-11-20 16:48:35 +0900

    Update train.py

--- run/train.py
+++ run/train.py
@@ -10,7 +10,8 @@ from pytorch_lightning.callbacks import (
     RichModelSummary,
     RichProgressBar,
 )
-from pytorch_lightning.loggers import WandbLogger
+#from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.loggers import TensorBoardLogger
 
 from src.conf import TrainConfig
 from src.datamodule import SleepDataModule
@@ -46,11 +47,16 @@ def main(cfg: TrainConfig):
     model_summary = RichModelSummary(max_depth=2)
 
     # init experiment logger
-    pl_logger = WandbLogger(
-        name=cfg.exp_name,
-        project="child-mind-institute-detect-sleep-states",
+    pl_logger = TensorBoardLogger(
+    save_dir="logs",  # ログを保存するディレクトリ。適切なパスに変更してください。
+    name=cfg.exp_name,
     )
-    pl_logger.log_hyperparams(cfg)
+    
+    # pl_logger = WandbLogger(
+    #     name=cfg.exp_name,
+    #     project="child-mind-institute-detect-sleep-states",
+    # )
+    # pl_logger.log_hyperparams(cfg)
 
     trainer = Trainer(
         # env
