0a11075ccdfc4f0987d026f7a75d832cc575169b
daichi1412 <149953061+daichi1412@users.noreply.github.com>
2023-11-20 16:55:52 +0900

    Update prepare_data.py

--- run/prepare_data.py
+++ run/prepare_data.py
@@ -20,21 +20,62 @@ SERIES_SCHEMA = {
 FEATURE_NAMES = [
     "anglez",
     "enmo",
-    "step",
+    #"step",
     "hour_sin",
     "hour_cos",
-    "month_sin",
-    "month_cos",
-    "minute_sin",
-    "minute_cos",
-    "anglez_sin",
-    "anglez_cos",
+    # "month_sin",
+    # "month_cos",
+    # "minute_sin",
+    # "minute_cos",
+    # "anglez_sin",
+    # "anglez_cos",
+    "signal_awake",
+    "signal_onset",
 ]
 
 ANGLEZ_MEAN = -8.810476
 ANGLEZ_STD = 35.521877
 ENMO_MEAN = 0.041315
 ENMO_STD = 0.101829
+
+# 新しい特徴量の辞書を定義
+onset_features = {
+    'w1': 0.97292814,
+    'mu1': 2.12861738,
+    'sigma1': 1.699476,
+    'mu2': 22.7643724,
+    'sigma2': 0.42483832
+}
+
+awake_features = {
+    'w1': 0.47799486,
+    'mu1': 10.89539674,
+    'sigma1': 0.87151052,
+    'mu2': 11.82689931,
+    'sigma2': 2.06792452
+}
+
+# 3次多項式のパラメータ
+#coefficients = [0.7498337722857453, -0.29120336781669365, 0.0325499456141388, -0.000865744935362603]
+
+# # 3次多項式の値を計算する関数を定義
+# def poly_fit(time):
+#     return coefficients[0] + coefficients[1] * time + coefficients[2] * time**2 + coefficients[3] * time**3
+
+def normal_pdf(x, mean, std):
+    sqrt_term = std * (2 * np.pi) ** 0.5
+    return (1 / sqrt_term) * ((-0.5 * ((x - mean) / std) ** 2).exp())
+
+
+def calc_mixture_gaussian(time, w1, mu1, sigma1, mu2, sigma2):
+    gaussian1 = normal_pdf(time, mu1, sigma1)
+    gaussian2 = normal_pdf(time, mu2, sigma2)
+    return w1 * gaussian1 + (1 - w1) * gaussian2
+
+
+# def calc_mixture_gaussian(time, w1, mu1, sigma1, mu2, sigma2):
+#     return w1 * norm.pdf(time, mu1, sigma1) + (1 - w1) * norm.pdf(time, mu2, sigma2)
+
 
 
 def to_coord(x: pl.Expr, max_: int, name: str) -> list[pl.Expr]:
@@ -45,24 +86,43 @@ def to_coord(x: pl.Expr, max_: int, name: str) -> list[pl.Expr]:
     return [x_sin.alias(f"{name}_sin"), x_cos.alias(f"{name}_cos")]
 
 
-def deg_to_rad(x: pl.Expr) -> pl.Expr:
-    return np.pi / 180 * x
+# def deg_to_rad(x: pl.Expr) -> pl.Expr:
+#     return np.pi / 180 * x
 
 
+# def add_feature(series_df: pl.DataFrame) -> pl.DataFrame:
+#     series_df = (
+#         series_df.with_row_count("step")
+#         .with_columns(
+#             *to_coord(pl.col("timestamp").dt.hour(), 24, "hour"),
+#             *to_coord(pl.col("timestamp").dt.month(), 12, "month"),
+#             *to_coord(pl.col("timestamp").dt.minute(), 60, "minute"),
+#             pl.col("step") / pl.count("step"),
+#             pl.col('anglez_rad').sin().alias('anglez_sin'),
+#             pl.col('anglez_rad').cos().alias('anglez_cos'),
+#         )
+#         .select("series_id", *FEATURE_NAMES)
+#     )
+#     return series_df
+
+# add_feature 関数に新しい特徴量を計算するコードを組み込む
 def add_feature(series_df: pl.DataFrame) -> pl.DataFrame:
-    series_df = (
-        series_df.with_row_count("step")
-        .with_columns(
-            *to_coord(pl.col("timestamp").dt.hour(), 24, "hour"),
-            *to_coord(pl.col("timestamp").dt.month(), 12, "month"),
-            *to_coord(pl.col("timestamp").dt.minute(), 60, "minute"),
-            pl.col("step") / pl.count("step"),
-            pl.col('anglez_rad').sin().alias('anglez_sin'),
-            pl.col('anglez_rad').cos().alias('anglez_cos'),
-        )
-        .select("series_id", *FEATURE_NAMES)
+    # 既存の特徴量の計算
+    series_df = series_df.with_columns(
+        *to_coord(pl.col("timestamp").dt.hour(), 24, "hour"),
     )
-    return series_df
+    
+    # hour_plus_minute を計算
+    hour_plus_minute = (pl.col("timestamp").dt.hour() * 10 + pl.col("timestamp").dt.minute() // 6) / 10
+
+    # ベクトル化された操作で新しい特徴量を計算
+    series_df = series_df.with_columns([
+        calc_mixture_gaussian(hour_plus_minute, **awake_features).alias("signal_awake"),
+        calc_mixture_gaussian(hour_plus_minute, **onset_features).alias("signal_onset")
+    ])
+    
+    return series_df.select("series_id", *FEATURE_NAMES)
+#poly_fit(hour_plus_minute).alias("signal_poly")
 
 
 def save_each_series(this_series_df: pl.DataFrame, columns: list[str], output_dir: Path):
@@ -111,7 +171,7 @@ def main(cfg: PrepareDataConfig):
                     pl.col("anglez"),
                     pl.col("enmo"),
                     pl.col("timestamp"),
-                    pl.col("anglez_rad"),
+                    #pl.col("anglez_rad"),
                 ]
             )
             .collect(streaming=True)
