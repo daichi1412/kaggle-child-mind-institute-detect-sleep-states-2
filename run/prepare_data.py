ce0dc8c49da4866649abc12125a22d72ead463e4
daichi1412 <149953061+daichi1412@users.noreply.github.com>
2023-11-20 22:46:51 +0900

    Update prepare_data.py

--- run/prepare_data.py
+++ run/prepare_data.py
@@ -23,17 +23,17 @@ FEATURE_NAMES = [
     "anglez",
     "enmo",
     #"step",
-    "hour_sin",
-    "hour_cos",
+    # "hour_sin",
+    # "hour_cos",
     # "month_sin",
     # "month_cos",
     # "minute_sin",
     # "minute_cos",
     # "anglez_sin",
     # "anglez_cos",
-    "signal_awake",
-    "signal_onset",
-    "anglez_savgolFilter_300"
+    # "signal_awake",
+    # "signal_onset",
+    # "anglez_savgolFilter_300"
 ]
 
 ANGLEZ_MEAN = -8.810476
